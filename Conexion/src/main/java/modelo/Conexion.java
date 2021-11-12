f7d0d5a87222cf0fc304f3b3d732ba9492d96503
VGaramendi <94187204+VGaramendi@users.noreply.github.com>
2021-11-12 11:33:04 -0500

    Add files via upload

---
+++ Conexion/src/main/java/modelo/Conexion.java
@@ -0,0 +1,18 @@
+//@Autor VIVIANA GARAMENDI
+package modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class Conexion {
+String url="jdbc:mysql://localhost:3306/prueba";
+String user="root",pass="";
+Connection con;
+public Connection getConnection(){
+try {
+//Para hacer la conexión
+Class.forName("com.mysql.cj.jdbc.Driver");
+con=DriverManager.getConnection(url,user,pass);
+} catch (Exception e) {
+}
+return con;
+}
+}
